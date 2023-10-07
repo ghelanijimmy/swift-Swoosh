d6fa63daf3712ee6fb4ee77691c48dfa49f071f1
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-10-07 10:30:56 -0400

    Initial Commit

---
+++ Swoosh/SwooshApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwooshApp.swift
+//  Swoosh
+//
+//  Created by Jimmy Ghelani on 2023-10-07.
+//
+
+import SwiftUI
+
+@main
+struct SwooshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
